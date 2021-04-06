8548a83fb47e46ae377c19affc94c54fb9ea5769
mtakaji <mauriciotakaji@yahoo.com>
2021-04-06 17:50:11 -0300

    criei module game

---
+++ tutorial.ex
@@ -0,0 +1,22 @@
+defmodule Game do
+  def play do
+  hints = "Feio, Gordo , Cabecudo"
+  IO.puts "Hints: #{hints}"
+
+  guess = IO.gets "Qual outra caracteritisca do Hugo?"
+  guess = String.strip(guess)
+  
+  attempt(guess)
+end
+  
+  def attempt("viado") do
+  IO.puts "parabens, voce ganhou!"
+end
+  def attempt(wrong_guess) do
+  IO.puts "nao eh essa!"
+  
+  guess = IO.gets "Tente de novo, burro!"
+  guess = String.strip(guess)
+  attempt(guess)
+  end
+end
