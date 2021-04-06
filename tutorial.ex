f8ab25077a39b6c696b597d94870f7ee012f1a6d
mtakaji <mauriciotakaji@yahoo.com>
2021-04-06 18:20:22 -0300

    hello mundo

--- tutorial.ex
+++ tutorial.ex
@@ -1,21 +1,4 @@
-defmodule Game do
-  def play do
-  hints = "bonito, magro , Cabecudo"
-  IO.puts "Hints: #{hints}"
-
-  guess = IO.gets "Qual outra caracteritisca do Hugo?"
-  guess = String.strip(guess)
-  
-  attempt(guess)
-end
-   def attempt(wrong_guess) do
-  IO.puts "nao eh essa!"
-end
-  def attempt(wrong_guess) do
-  IO.puts "nao eh essa!"
-  
-  guess = IO.gets "Tente de novo, burro!"
-  guess = String.strip(guess)
-  attempt(guess)
-  end
-end
+defmodule Hello do
+ def hello do
+  "hello world"
+   end
