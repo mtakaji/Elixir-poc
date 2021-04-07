9deeeb5a558017fa8c28422a06e8a2e2d2354e24
mtakaji <mauriciotakaji@yahoo.com>
2021-04-07 14:26:00 -0300

    atualizando metodo do tutorial.ex

--- tutorial.ex
+++ tutorial.ex
@@ -1,22 +1,22 @@
 defmodule Game do
   def play do
-  hints = "bonito, magro , Cabecudo"
-  IO.puts "Hints: #{hints}"
+    hints = "bonito, magro , Cabecudo"
+    IO.puts "Hints: #{hints}"
 
-  guess = IO.gets "Qual outra caracteritisca do Hugo?"
-  guess = String.strip(guess)
-  
-  attempt(guess)
-end
-   def attempt(wrong_guess) do
-  IO.puts "nao eh essa!"
-end
+    guess = IO.gets "Qual outra caracteritisca do Hugo?"
+    guess = String.strip(guess)
+
+    attempt(guess)
+  end
+
+  def attempt("gordo") do
+    IO.puts "voce venceu!"
+  end
   def attempt(wrong_guess) do
-  IO.puts "nao eh essa!"
-  
-  guess = IO.gets "Tente de novo, burro!"
-  guess = String.strip(guess)
-  attempt(guess)
+    IO.puts "nao eh essa!"
+
+    guess = IO.gets "Tente de novo, burro!"
+    guess = String.strip(guess)
+    attempt(guess)
   end
 end
-.
